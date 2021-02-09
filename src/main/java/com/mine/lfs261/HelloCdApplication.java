dfe61ec022e705478bf40ce6d8d69608c5450ca6
Alberto Farias <cafaray@gmail.com>
2021-02-09 19:25:52 +0100

    First change to webapp branch

--- src/main/java/com/mine/lfs261/HelloCdApplication.java
+++ src/main/java/com/mine/lfs261/HelloCdApplication.java
@@ -1,10 +1,18 @@
 package com.mine.lfs261;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloCdApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloCdApplication.class, args);
