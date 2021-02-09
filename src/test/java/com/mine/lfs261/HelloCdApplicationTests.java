4ebfa04959d9bcb40e5437defff89d2e8565363a
Alberto Farias <cafaray@gmail.com>
2021-02-09 15:15:18 +0100

    adding pom.xml specs

---
+++ src/test/java/com/mine/lfs261/HelloCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mine.lfs261;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
